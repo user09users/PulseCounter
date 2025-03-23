0fcb664cf4923a9ef3b31aa8140d2c15d16c2fee
Mkh <example@gmail.com>
2025-03-23 20:08:46 +0100

    add slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 import "/src/sass/style.scss";
+
+$(document).ready(function () {
+    $('.carousel__inner').slick({
+        infinite: true,
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="/src/img/slides/left.png" alt="left" /></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="/src/img/slides/right.png" alt="right" /></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    arrows: false,
+                    dots: true,
+                }
+            }
+        ]
+    });
+});
