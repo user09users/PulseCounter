b1e9f4bf7a8f9bd05aedc593ae086cced907421d
Mkh <example@gmail.com>
2025-03-25 20:15:06 +0100

    add reviews

--- src/js/script.js
+++ src/js/script.js
@@ -18,14 +18,28 @@ document.querySelector('.prev').addEventListener('click', () => {
 document.querySelector('.next').addEventListener('click', () => {
     slider.goTo('next');
 });
-
+/*  
 $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
     $(this)
         .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_activee')
         .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('active');
 });
 
-/* 
+
+function toggleClass(item) {
+    $(item).each(function (i) {
+        $(this).on('click', function (e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__content_active');
+        })
+    })
+}
+
+toggleClass('.catalog-item__link');
+toggleClass('.catalog-item__back');
+
+
 $(document).ready(function () {
     $('.carousel__inner').slick({
         infinite: true,
